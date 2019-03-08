dd9861cc08633eec1e6ee350920b54291e8ff28c
zwp <1258271806@qq.com>
2019-03-08 13:03:47 +0800

    init

---
+++ src/com/huawei/MainTest.java
@@ -0,0 +1,18 @@
+package com.huawei;
+
+import com.huawei.app.Application;
+
+public class MainTest {
+
+	static String basePath = "E:/huawei2019/train";
+    static String carPath = basePath+"/Car.txt";
+    static String roadPath = basePath+"/Road.txt";
+    static String crossPath = basePath+"/Cross.txt";
+    static String answerPath = basePath+"/answer.txt";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.run(new String[]{carPath,roadPath,crossPath,answerPath});
+	}
+
+}
---
+++ src/com/huawei/app/Application.java
@@ -0,0 +1,9 @@
+package com.huawei.app;
+
+public class Application {
+
+	public static void run(String... args) {
+		
+	}
+	
+}
