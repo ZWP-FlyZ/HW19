5b9cfaf41cd1c9eb9e9009e2ba3048291b79fc10
zwp <1258271806@qq.com>
2019-03-08 13:10:34 +0800

    更改编码UTF8

--- src/com/huawei/MainTest.java
+++ src/com/huawei/MainTest.java
@@ -13,6 +13,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Application.run(new String[]{carPath,roadPath,crossPath,answerPath});
+		
 	}
 
 }
