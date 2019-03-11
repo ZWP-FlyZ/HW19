011ecc7425682bd8d134f30c2459731598e27307
zwp <1258271806@qq.com>
2019-03-11 19:40:48 +0800

    更新规划器

--- src/com/huawei/app/PathPlanner.java
+++ src/com/huawei/app/PathPlanner.java
@@ -1,7 +1,10 @@
 package com.huawei.app;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 
@@ -18,13 +21,16 @@ public class PathPlanner implements Planner {
     private Map<Integer,Car> cars = null;
     private Map<Integer,Road> roads = null;
     private Map<Integer,Cross> crosses = null;
-    
+    private Map<Integer,Integer> crossReIdx=null;
+    private List<Integer>  crossIdx =null;
+    private Road[][] graph = null;
     
 	public PathPlanner(Context ctx){
     	this.ctx= ctx; 
     	cars = ctx.cars;
     	roads=ctx.roads;
     	crosses=ctx.crosses;
+    	createCrossIdx(crosses.keySet());
 	}
 	
 	
@@ -32,8 +38,51 @@ public class PathPlanner implements Planner {
 	 *  初始化将所有车初始方法缓存
 	 */
 	public void init() {
-		
+		// 初始化图
+		graph = new Road[crosses.size()][crosses.size()];
+		roads.values().forEach(road->{
+			int i=cIdx(road.getFromCrossId());
+			int j=cIdx(road.getToCrossId());
+			graph[i][j]=road;
+			if(road.isDuplex()) 
+				graph[j][i]=road;
+		});
+		System.err.println("");
 	}
+	
+	
+	private void createCrossIdx(Collection<Integer> crossIds){
+		Map<Integer,Integer> res = new HashMap<>();
+		List<Integer> ids = crossIds.stream()
+			.sorted((a,b)->Integer.compare(a, b))
+			.collect(Collectors.toList());
+		for(int i=0;i<ids.size();i++) {
+			res.put(ids.get(i), i);
+		}
+		crossIdx = ids;
+		crossReIdx = res;
+	}
+	
+	/**
+	 * idx
+	 * @param crossId
+	 * @return
+	 */
+	private int cIdx(int crossId) {
+		return crossReIdx.get(crossId);
+	}
+	
+	/**
+	 * 返回利用当前耗费
+	 * @param road
+	 * @param car
+	 * @return
+	 */
+	private int cost(Road road,Car car) {
+		int mxspd = Math.min(road.getMaxSpeed(), car.getMaxSpeed());
+		return (int)Math.ceil(road.getRoadLength()*1.0/mxspd);
+	}
+	
 	
 	
 	@Override
